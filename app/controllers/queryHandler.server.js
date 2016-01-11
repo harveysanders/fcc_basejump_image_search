21e7e37283c9638e859fb5999633f6dcc99b12e4
Harvey Sanders <sanders.harvey@gmail.com>
2016-01-11 00:43:43 -0600

    add support for Bing Search API

--- app/controllers/queryHandler.server.js
+++ app/controllers/queryHandler.server.js
@@ -1,30 +1,53 @@
 'use strict';
 
 var Queries = require('../models/queries.js');
-// imageSearch results = [
-// 	{
-// 		url: '...',
-// 		snippet: 'lol catz breaks ...',
-// 		thumbnail: '...jpg',
-// 		context: 'hosting site url'
-// 	},
-//	{...},
-//	{...}
-// ]
+var request = require('request');
 
-// example latest searches = [
-// 	{"term":"lolcats funny","when":"2016-01-07T22:30:56.731Z"},
-// 	{"term":"lolcats funny","when":"2016-01-07T22:12:31.928Z"},
-// 	{"term":"hello","when":"2016-01-07T22:03:49.566Z"},
-//	{...}
-// ]
+require('dotenv').load();
 
 function queryHandler() {
 	return {
 		imageSearch: function(req, res) {
 			var query = req.params.query;
+			var skip = (req.query.offset - 1) * 50 || 0;
 
-			//TODO: figure out search
+			//Bing Search API
+			var acctKey = process.env.BING_API_KEY;
+			var rootUri = 'https://api.datamarket.azure.com';
+			var auth    = new Buffer(acctKey + ':' + acctKey).toString('base64');
+			
+			var q = encodeURIComponent(query);
+
+			var bingOpts = {
+				url: rootUri + '/Bing/Search/Image?$format=json&Query=%27' + q + '%27&$skip=' + skip,
+				headers: {'Authorization' : 'Basic ' + auth}
+			};
+
+			console.log('url: ' + bingOpts.url);
+			request(bingOpts, function(error, response, body) {
+				if (error) {
+					console.log(error);
+				}
+				console.log('statusCode: ' + response.statusCode);
+				console.log('headers: ' + JSON.stringify(response.headers));
+				 if (!error && response.statusCode === 200) {
+				    var results = JSON.parse(body);
+				    console.log(results);
+
+				    var formattedResults = results.d.results.map(function(searchRes) {
+				    	return {
+				    		url: searchRes.MediaUrl,
+							snippet: searchRes.Title,
+							thumbnail: searchRes.Thumbnail.MediaUrl,
+							context: searchRes.SourceUrl
+				    	};
+				    });
+
+				   	res.json(formattedResults);
+				  }
+			});
+
+			//keep record of searches
 			var newQuery = new Queries({
 				queryTerm: query,
 				date: Date.now()
@@ -32,7 +55,8 @@ function queryHandler() {
 
 			newQuery.save(function (err, newQuery, numAffected) {
 				if (err) throw err;
-				res.json(newQuery);
+				console.log(newQuery.queryTerm + ' searched at ' + newQuery.date);
+				// res.send(newQuery);
 			});
 		},
 
@@ -40,6 +64,7 @@ function queryHandler() {
 
 			Queries
 				.find({})
+				.sort({'date': -1}) //newest first
 				.limit(10)
 				.exec( function(err, queries) {
 					if (err) res.send(err);
@@ -51,7 +76,6 @@ function queryHandler() {
 						};
 					}));		
 				});
-			
 		}
 	};
 }
